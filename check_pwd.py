09178fa1f4896a3a3db9cff47b9a5cb59e410157
Victoria Zavala <71748624+vzavala95@users.noreply.github.com>
2022-02-12 16:49:27 -0800

    Commit 3: Modified check_pwd to reject empty string and return False. Otherwise return True

--- check_pwd.py
+++ check_pwd.py
@@ -1,6 +1,5 @@
-# References:
-#1. https://edstem.org/us/courses/16737/discussion/1120959
-
 def check_pwd(nums):
     # will return True no matter what
-    return False
+    if nums == "":
+        return False
+    return True
