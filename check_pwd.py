d92826f258a46bbaec14317635c5f70997218c53
Victoria Zavala <71748624+vzavala95@users.noreply.github.com>
2022-02-12 16:56:16 -0800

    Commit 5: Modified check_pwd to reject any password length < 8 characters. All tests now pass

--- check_pwd.py
+++ check_pwd.py
@@ -2,4 +2,11 @@ def check_pwd(nums):
     # will return True no matter what
     if nums == "":
         return False
+    pwd_length = len(nums)
+
+    if pwd_length < 8:
+        # password cannot be less than 8 characters long
+        return False
     return True
+
+
