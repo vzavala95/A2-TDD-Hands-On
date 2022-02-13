28342192c056563dce04f01913d0f16a80e216a6
Victoria Zavala <71748624+vzavala95@users.noreply.github.com>
2022-02-12 17:33:37 -0800

    Commit 9: Modified check_pwd and added a function that tests if a password does not contain any uppercase characters. All tests pass now

--- check_pwd.py
+++ check_pwd.py
@@ -1,15 +1,21 @@
 def check_pwd(nums):
     # will return True no matter what
-    if nums == "":
-        return False
+    value = True
     pwd_length = len(nums)
+    has_upper = any(x.isupper() for x in nums)
+    if nums == "":
+        value = False
 
     if pwd_length < 8:
         # password cannot be less than 8 characters long
-        return False
+        value = False
+
     if pwd_length > 20:
-        # password length cannot exceed 20 characters 
-        return False
-    return True
+        # password length cannot exceed 20 characters
+        value = False
+    if not has_upper:
+        value = False
+    if value:
+        return True
 
 
