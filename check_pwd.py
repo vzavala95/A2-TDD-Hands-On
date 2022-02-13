280a000c27d561fe5f4e7da950fda3aec0e9e2a9
Victoria Zavala <71748624+vzavala95@users.noreply.github.com>
2022-02-12 17:03:08 -0800

    Commit 7: Modified check_pwd to reject password lengths of > 20 characters. All tests passing now

--- check_pwd.py
+++ check_pwd.py
@@ -7,6 +7,9 @@ def check_pwd(nums):
     if pwd_length < 8:
         # password cannot be less than 8 characters long
         return False
+    if pwd_length > 20:
+        # password length cannot exceed 20 characters 
+        return False
     return True
 
 
